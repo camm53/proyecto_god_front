810c24f78978335c82d15a9da78d9b7d17c5db47
camm35 <cammcontact.official@gmail.com>
2024-10-20 19:54:07 -0500

    se actualiza la navegacion 3/3

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = ()=> {
       
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden" >
         <Button className="mt-10">
-          segundo intento
+          1
         </Button>
         <Button className="mt-10" href="#login">
           something
